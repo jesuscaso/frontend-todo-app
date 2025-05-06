98d1d64a512cf7a0a28394b56954e2adfcc642f0
jesuscaso <jesus.caso@gmail.com>
2025-05-06 12:59:40 +0200

    actualización footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>
-                Desarrollado por Nombre del Alumno · Curso 2025 &nbsp;
+                Desarrollado por Jesús Caso · Curso 2025 &nbsp;
                 <a
                     href="https://github.com/idoia-durana"
                     target="_blank"
